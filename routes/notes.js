3fd75fd6e510e7b0e2d39291dc21f7c92336073f
naman0058 <jnaman345@gmail.com>
2020-04-24 16:08:49 +0530

    a

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('./pool')
-var table = 'programming_language'
+var table = 'notes'
 
 router.get('/', (req, res) =>  res.render(`notes`));
+
+
+
+
+router.post('/insert', (req, res) => pool.query(`update ${table} set download = download+1` , (err, result) => err ? console.log(err) : res.json(result)))
 
 
 module.exports = router;
