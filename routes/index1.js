e4f9ee0733ac7dfb49a6ef663404a01cc893aa0f
naman0058 <jnaman345@gmail.com>
2020-07-07 16:14:30 +0530

    a

--- routes/index1.js
+++ routes/index1.js
@@ -4,14 +4,8 @@ var router = express.Router();
 var mysql = require('mysql')
 var pool = require('./pool')
 
-
-router.get('/', (req, res) => {
-    if(req.session.id) {
-      res.render(`index1`, { login: true });
-    }
-    else {
-      res.render(`index1`, { login: false });
-    }
+router.get('/', (req, res) => { pool.query(`select name,seo_name,short_description from project`,
+(err,result)=>err ? console.log(err) : res.render('index1',{result:result}))
 })
 
 
