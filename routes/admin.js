003ceff559ac5a618b77c0d9b5a192780c17b13e
naman0058 <jnaman345@gmail.com>
2020-10-20 15:18:31 +0530

    a

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 
 var express = require('express');
 var router = express.Router();
+var upload = require('./multer');
 var mysql = require('mysql')
 var pool = require('./pool')
 var table = 'admin'
+var table1 = 'add_project'
 require('dotenv').config()
 router.get('/', (req, res) => {
     res.render(`admin`,{msg : ''});
@@ -29,6 +31,40 @@ router.post('/login',(req,res)=>{
     else res.render(`admin`,{msg : 'API KEY Is Invalid'})
 })
 
+
+
+
+
+
+router.get('/add-project',(req,res)=>{
+    pool.query(`select * from ${table1}`,(err,result)=>{
+        if(err) throw err;
+        else res.render('AddProject/add-project' , {result})
+    })
+    
+})
+
+
+
+
+
+
+
+
+router.post('/add-project/insert',upload.single('zip'),(req,res)=> {
+                                         let body = req.body;  
+                                         var dirt = false
+                                         var seo_variable = (body.name.split(' ').join('-')).toLowerCase()
+                                         body['source_code'] = req.file.filename
+                                         body['seo_name'] = seo_variable 
+                                         pool.query(`insert into ${table1} set ?`,body,(err,result)=>err ? console.log(err) : res.json(result))
+
+});
+
+
+
+
+router.get('/project-delete', (req, res) => pool.query(`delete from ${table1} where id = ${req.query.id}`, (err, result) => err ? console.log(err) : res.json(result)))
 
 
 module.exports = router;
