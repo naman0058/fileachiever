b57091825fdc59798f40fea22803487cca8ab43a
naman0058 <jnaman345@gmail.com>
2023-02-11 10:37:46 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -390,25 +390,27 @@ response.redirect(`https://www.filemakr.com/btech-final-year-project-report/proj
             pool.query(`select * from payment_request where order_id = '${request.body.orderNo}'`,(err,result)=>{
                 if(err) throw err;
                 else {
-                    let source_code_id= result[0].source_code_id
-console.log('payment request data',result[0].source_code_id);
-                    pool.query(`select * from btech_project where id = '${source_code_id}'`,(err,result)=>{
-                        if(err) throw err;
-                        else {
+//                     let source_code_id= result[0].source_code_id
+// console.log('payment request data',result[0].source_code_id);
+//                     pool.query(`select * from btech_project where id = '${source_code_id}'`,(err,result)=>{
+//                         if(err) throw err;
+//                         else {
                      
-                           var query = `select * from btech_project where id = '${source_code_id}';`
-                           var query1 = `select * from programming_language where id = '${result[0].html}' or id = '${result[0].css}' or id = '${result[0].bootstrap}' or id = '${result[0].javascript}' or id = '${result[0].jquery}' or id = '${result[0].json}' or id = '${result[0].react}' or id = '${result[0].angular}'  ;`
-                           var query2 = `select * from programming_language where id = '${result[0].php}' or id = '${result[0].nodejs}' or id = '${result[0].python}' or id = '${result[0].java}';`
-                           var query3 = `select * from project where id = '${result[0].projectid}';`
-                         //For Testing
+//                            var query = `select * from btech_project where id = '${source_code_id}';`
+//                            var query1 = `select * from programming_language where id = '${result[0].html}' or id = '${result[0].css}' or id = '${result[0].bootstrap}' or id = '${result[0].javascript}' or id = '${result[0].jquery}' or id = '${result[0].json}' or id = '${result[0].react}' or id = '${result[0].angular}'  ;`
+//                            var query2 = `select * from programming_language where id = '${result[0].php}' or id = '${result[0].nodejs}' or id = '${result[0].python}' or id = '${result[0].java}';`
+//                            var query3 = `select * from project where id = '${result[0].projectid}';`
+//                          //For Testing
                 
-                           pool.query(query+query1+query2+query3,(err,result)=>{
-                               if(err) throw err;
-                               else response.render('B.Tech/finalnew',{result:result})
-                           })
+//                            pool.query(query+query1+query2+query3,(err,result)=>{
+//                                if(err) throw err;
+//                                else response.render('B.Tech/finalnew',{result:result})
+//                            })
                 
-                        }
-                    })
+//                         }
+//                     })
+
+response.redirect('/download-project-report')
                 }
 
             })
@@ -430,6 +432,70 @@ console.log('payment request data',result[0].source_code_id);
 
 })
 
+
+
+router.get('/download-project-report',(req,res)=>{
+    if(req.session.roll_number){
+
+
+        if(req.session.deviceInfo == 'mobile'){
+        
+        
+        
+        
+          pool.query(`select * from btech_project where roll_number = '${req.session.roll_number}' order by id desc limit 1`,(err,result)=>{
+                if(err) throw err;
+                else {
+                    console.log(req.session.roll_number)
+                    console.log(result[0].php)
+                   var query = `select * from ${table} where roll_number = '${req.session.roll_number}' order by id desc limit 1;`
+                   var query1 = `select * from programming_language where id = '${result[0].html}' or id = '${result[0].css}' or id = '${result[0].bootstrap}' or id = '${result[0].javascript}' or id = '${result[0].jquery}' or id = '${result[0].json}' or id = '${result[0].react}' or id = '${result[0].angular}'  ;`
+                   var query2 = `select * from programming_language where id = '${result[0].php}' or id = '${result[0].nodejs}' or id = '${result[0].python}' or id = '${result[0].java}';`
+                   var query3 = `select * from project where id = '${result[0].projectid}';`
+                   //For Testing
+        
+                   pool.query(query+query1+query2+query3,(err,result)=>{
+                       if(err) throw err;
+                       //else res.json(result)
+                       else res.render('B.Tech/finalnew',{result:result})
+                   })
+        
+                }
+            })
+        
+        
+        
+        }
+        else{
+        
+        
+          pool.query(`select * from btech_project where roll_number = '${req.session.roll_number}' order by id desc limit 1`,(err,result)=>{
+                if(err) throw err;
+                else {
+                    console.log(req.session.roll_number)
+                    console.log(result[0].php)
+                   var query = `select * from ${table} where roll_number = '${req.session.roll_number}' order by id desc limit 1;`
+                   var query1 = `select * from programming_language where id = '${result[0].html}' or id = '${result[0].css}' or id = '${result[0].bootstrap}' or id = '${result[0].javascript}' or id = '${result[0].jquery}' or id = '${result[0].json}' or id = '${result[0].react}' or id = '${result[0].angular}'  ;`
+                   var query2 = `select * from programming_language where id = '${result[0].php}' or id = '${result[0].nodejs}' or id = '${result[0].python}' or id = '${result[0].java}';`
+                   var query3 = `select * from project where id = '${result[0].projectid}';`
+                 //For Testing
+        
+                   pool.query(query+query1+query2+query3,(err,result)=>{
+                       if(err) throw err;
+                       else res.render('B.Tech/finalnew',{result:result})
+                   })
+        
+                }
+            })
+        
+        }
+        
+        
+        }
+        else{
+            res.redirect('/')
+        }
+})
 
 
 
