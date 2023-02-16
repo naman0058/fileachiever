8c7b37f585eb1a37b404ef28e682d2973a7c099b
naman0058 <jnaman345@gmail.com>
2023-02-16 14:20:18 +0530

    a

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ extended: false, limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }))
 
 
 app.get('/events', function (req, res) {
