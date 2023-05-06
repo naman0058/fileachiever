f8f2507155d64c24dc2b231b2ba28db0ada6df36
naman0058 <jnaman345@gmail.com>
2023-05-06 08:21:29 +0530

    a

--- app.js
+++ app.js
@@ -151,13 +151,6 @@ app.use(function(err, req, res, next) {
 
 
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-  });
 
 
 module.exports = app;
