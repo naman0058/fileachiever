4246f7ee84a72ffedc0cc0570c333cd4f239e2f5
naman0058 <jnaman345@gmail.com>
2023-05-06 08:17:26 +0530

    a

--- app.js
+++ app.js
@@ -151,16 +151,13 @@ app.use(function(err, req, res, next) {
 
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
+  //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  if ('OPTIONS' == req.method) {
-     res.sendStatus(200);
-   }
-   else {
-     next();
-   }});
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 
 
 module.exports = app;
