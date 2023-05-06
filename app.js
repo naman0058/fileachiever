6ecd049a823349a62191c1d088a5c3fe1cb28936
naman0058 <jnaman345@gmail.com>
2023-05-06 08:11:01 +0530

    a

--- app.js
+++ app.js
@@ -147,4 +147,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if ('OPTIONS' == req.method) {
+     res.sendStatus(200);
+   }
+   else {
+     next();
+   }});
+
+   
 module.exports = app;
